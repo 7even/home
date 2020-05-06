6ddb44b524d306cd62d614fdf6c679a5b754997f
Vsevolod Romashov <7@7vn.ru>
2020-05-06 18:14:12 +0300

    add integrant and a basic :http/handler component

--- src/clj/home/core.clj
+++ src/clj/home/core.clj
@@ -1,10 +1,15 @@
 (ns home.core
   (:require [aero.core :as aero]
-            [clojure.java.io :refer [resource]]))
+            [clojure.java.io :refer [resource]]
+            [home.http :as http]
+            [integrant.core :as ig]))
 
 (def app-env
   (or (keyword (System/getenv "APP_ENV"))
       :development))
+
+(defmethod aero/reader 'ig/ref [_ _ value]
+  (ig/ref value))
 
 (defn config
   ([] (config app-env))
@@ -12,3 +17,17 @@
    (-> "config.edn"
        resource
        (aero/read-config {:profile profile}))))
+
+(defmethod ig/init-key :http/handler [_ config]
+  (println ";; Starting HTTP handler")
+  (http/start config))
+
+(defmethod ig/halt-key! :http/handler [_ server]
+  (println ";; Stopping HTTP handler")
+  (http/stop server))
+
+(defn -main []
+  (let [system-config (assoc-in (config)
+                                [:http/handler :join?]
+                                true)]
+    (ig/init system-config)))
