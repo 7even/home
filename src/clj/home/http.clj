3a610b7be2d5b837e675425b4787452d291700dd
Vsevolod Romashov <7@7vn.ru>
2020-05-07 21:07:29 +0300

    add a simple websocket endpoint

--- src/clj/home/http.clj
+++ src/clj/home/http.clj
@@ -3,13 +3,28 @@
             [aleph.netty :refer [wait-for-close]]
             [compojure.core :refer [GET routes]]
             [home.html :as html]
+            [manifold.deferred :as d]
+            [manifold.stream :as s]
             [ring.middleware.resource :refer [wrap-resource]]
             [ring.util.response :refer [content-type response]]))
 
+(defn- send-to-ws [s payload]
+  (s/put! s (pr-str payload)))
+
+(defn ws-handler [req]
+  (d/let-flow [conn (http/websocket-connection req)]
+    (send-to-ws conn {:message "Hello!"})
+    (s/consume (fn [payload]
+                 (let [client-message (read-string payload)]
+                   (send-to-ws conn {:you/sent client-message})))
+               conn)
+    nil))
+
 (def app-routes
   (routes
-   (GET "/" [] (-> (response html/page)
-                   (content-type "text/html")))))
+   (GET "/ws" [] ws-handler)
+   (GET "/*" [] (-> (response html/page)
+                    (content-type "text/html")))))
 
 (def app
   (-> app-routes
