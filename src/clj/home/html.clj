005b62caa97771786c49e2afa97cd437a364ad9c
Vsevolod Romashov <7@7vn.ru>
2020-05-15 23:21:43 +0300

    add bootstrap

--- src/clj/home/html.clj
+++ src/clj/home/html.clj
@@ -5,7 +5,11 @@
   (html5
    [:head
     [:meta {:charset "utf-8"}]
-    [:title "Home dashboard"]]
+    [:title "Home dashboard"]
+    [:link {:rel "stylesheet"
+            :href "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+            :integrity "sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+            :crossorigin "anonymous"}]]
    [:body
     [:div#root]
     [:script {:src "/js/main.js"}]]))
--- src/cljs/home/views.cljs
+++ src/cljs/home/views.cljs
@@ -1,6 +1,21 @@
 (ns home.views
   (:require [re-frame.core :as rf]
+            [cljs-time.core :as t]
+            [cljs-time.format :as f]
             [home.subs :as subs]))
+
+(defn- format-date [date]
+  (let [formatter (f/formatter "HH:mm dd MMM yyyy")]
+    (f/unparse formatter (t/to-default-time-zone date))))
+
+(defn card [{:keys [title url image-url description published-at]} rss-name]
+  [:div.card.my-2 {:style {:width "36rem"}}
+   [:div.card-body.p-2
+    [:p.card-text title]
+    [:div.text-right
+     [:a {:href url
+          :target :blank}
+      (str rss-name " @ " (format-date published-at))]]]])
 
 (defn interface []
   [:div
@@ -8,10 +23,6 @@
    (for [{:rss/keys [id name news]} @(rf/subscribe [::subs/rss])]
      ^{:key id}
      [:div
-      [:h2 name]
-      [:ul
-       (for [{:keys [title url image-url description published-at]} news]
-         ^{:key url}
-         [:li [:a {:href url
-                   :target :blank}
-               title]])]])])
+      (for [item news]
+        ^{:key (:url item)}
+        [card item name])])])
