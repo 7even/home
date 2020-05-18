76ac68338576fec592685c3b5eb64626ff2e3c8d
Vsevolod Romashov <7@7vn.ru>
2020-05-18 22:12:02 +0300

    add :rss-items subscription

--- src/cljs/home/subs.cljs
+++ src/cljs/home/subs.cljs
@@ -1,6 +1,11 @@
 (ns home.subs
   (:require [re-frame.core :as rf]))
 
-(rf/reg-sub ::rss
+(rf/reg-sub ::rss-items
             (fn [db]
-              (:rss db)))
+              (->> (:rss db)
+                   (mapcat (fn [rss]
+                             (map #(assoc % :source (:rss/name rss))
+                                  (:rss/news rss))))
+                   (sort-by :published-at >)
+                   (take 30))))
--- src/cljs/home/views.cljs
+++ src/cljs/home/views.cljs
@@ -8,21 +8,16 @@
   (let [formatter (f/formatter "HH:mm dd MMM yyyy")]
     (f/unparse formatter (t/to-default-time-zone date))))
 
-(defn card [{:keys [title url image-url description published-at]} rss-name]
+(defn card [{:keys [title url image-url description published-at source]}]
   [:div.card.my-2 {:style {:width "36rem"}}
    [:div.card-body.p-2
     [:p.card-text title]
     [:div.text-right
      [:a {:href url
           :target :blank}
-      (str rss-name " @ " (format-date published-at))]]]])
+      (str source " @ " (format-date published-at))]]]])
 
 (defn interface []
   [:div
-   [:h1 "RSS"]
-   (for [{:rss/keys [id name news]} @(rf/subscribe [::subs/rss])]
-     ^{:key id}
-     [:div
-      (for [item news]
-        ^{:key (:url item)}
-        [card item name])])])
+   (for [item @(rf/subscribe [::subs/rss-items])]
+     ^{:key (:url item)} [card item])])
