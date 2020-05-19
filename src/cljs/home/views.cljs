7607dee14e4a3a9db501f4935e1d8d9df26bcd16
Vsevolod Romashov <7@7vn.ru>
2020-05-19 22:21:19 +0300

    rework the page layout

--- src/cljs/home/views.cljs
+++ src/cljs/home/views.cljs
@@ -8,16 +8,20 @@
   (let [formatter (f/formatter "HH:mm")]
     (f/unparse formatter (t/to-default-time-zone date))))
 
-(defn card [{:keys [title url image-url description published-at source]}]
-  [:div.card.my-2 {:style {:width "36rem"}}
-   [:div.card-body.p-2
-    [:p.card-text title]
-    [:div.text-right
-     [:a {:href url
-          :target :blank}
-      (str source " @ " (format-date published-at))]]]])
+(defn news-item [{:keys [title url image-url description published-at source]}]
+  [:li.list-group-item
+   [:div title]
+   [:div.float-right
+    [:a {:href url
+         :target :blank}
+     [:small (str source " @ " (format-date published-at))]]]])
 
 (defn interface []
-  [:div
-   (for [item @(rf/subscribe [::subs/rss-items])]
-     ^{:key (:url item)} [card item])])
+  [:div.container-fluid
+   [:div.row
+    [:div.col-3
+     [:ul.list-group.list-group-flush
+      [:li.list-group-item
+       [:button.btn.btn-primary "Manage RSS feeds"]]
+      (for [item @(rf/subscribe [::subs/rss-items])]
+        ^{:key (:url item)} [news-item item])]]]])
