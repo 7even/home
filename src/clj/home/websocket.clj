b886d4662b31130ba334812f9b2518efc40c6d3e
Vsevolod Romashov <7@7vn.ru>
2020-05-30 23:26:28 +0300

    add & use home.websocket/handle-client-message

--- src/clj/home/websocket.clj
+++ src/clj/home/websocket.clj
@@ -1,15 +1,52 @@
 (ns home.websocket
-  (:require [datomic.api :as d]
+  (:require [clojure.spec.alpha :as s]
+            [datomic.api :as d]
             [home.rss :as rss]
             [home.websocket.utils :refer [send-to-ws]]
             [manifold.bus :as bus]
             [manifold.stream :as ms]))
 
+(def commands
+  {:rss/synchronize rss/synchronize-rss})
+
+(s/def :command/id uuid?)
+(s/def :command/name (-> commands keys set))
+(s/def :command/data (s/or :map map?
+                           :vec vector?))
+
+(s/def ::client-message
+  (s/keys :req [:command/id :command/name :command/data]))
+
+(defn- parse-client-message [ws-conn message-str]
+  (try
+    (let [data (read-string message-str)
+          message (s/conform ::client-message data)]
+      (if (= message ::s/invalid)
+        (do
+          (send-to-ws ws-conn
+                      {:message/error (s/explain-str ::client-message data)})
+          nil)
+        message))
+    (catch RuntimeException e
+      (send-to-ws ws-conn
+                  {:message/error "Failed to parse the message"})
+      nil)))
+
+(defn- handle-client-message [config message-data]
+  (let [{:command/keys [id name data]} message-data
+        command (get commands name)]
+    (command config (second data) id)))
+
 (defn get-initial-state [db]
   {:state/data {:rss (rss/get-news db)}})
 
 (defn handle-connection [{:keys [ws-conn db-conn]
-                          {:keys [bus]} :broadcaster}]
+                          {:keys [bus]} :broadcaster
+                          :as config}]
   (send-to-ws ws-conn (get-initial-state (d/db db-conn)))
+  (ms/consume (fn [payload]
+                (when-let [message (parse-client-message ws-conn payload)]
+                  (handle-client-message config message)))
+              ws-conn)
   (ms/connect (bus/subscribe bus :events)
               ws-conn))
--- test/clj/home/websocket_test.clj
+++ test/clj/home/websocket_test.clj
@@ -1,9 +1,11 @@
 (ns home.websocket-test
   (:require [clojure.test :refer :all]
+            [home.db.rss :as db.rss]
             [home.rss :as rss]
             [home.test :refer :all]
             [home.websocket :as ws]
-            [home.websocket.utils :refer [take-from-ws]]))
+            [home.websocket.utils :refer [send-to-ws take-from-ws]]
+            [manifold.stream :as s]))
 
 (defn- ws-config []
   {:ws-conn @ws-conn
@@ -17,3 +19,46 @@
   (ws/handle-connection (ws-config))
   (is (= {:state/data {:rss (rss/get-news (db))}}
          (take-from-ws (server->client)))))
+
+(deftest parse-client-message-test
+  (testing "with invalid edn"
+    (ws/handle-connection (ws-config))
+    (take-from-ws (server->client))
+    (s/put! (client->server) "\"foo")
+    (is (= "Failed to parse the message"
+           (:message/error (take-from-ws (server->client))))))
+  (testing "with invalid message"
+    (ws/handle-connection (ws-config))
+    (take-from-ws (server->client))
+    (send-to-ws (client->server) {:foo :bar})
+    (is (some? (re-find #":command/id"
+                        (:message/error (take-from-ws (server->client))))))))
+
+(deftest handle-client-message-test
+  (ws/handle-connection (ws-config))
+  (take-from-ws (server->client))
+  (let [command-id (random-uuid)
+        _ (send-to-ws (client->server)
+                      {:command/id command-id
+                       :command/name :rss/synchronize
+                       :command/data [{:rss/name "Vedomosti"
+                                       :rss/url (local-file-url "vedomosti.xml")}]})
+        feeds (db.rss/list-rss (db))
+        feed (first feeds)
+        events-msg (take-from-ws (server->client))
+        event (-> events-msg :events first)]
+    (is (= 1 (count feeds)))
+    (is (= "Vedomosti"
+           (:rss/name feed)))
+    (is (= (local-file-url "vedomosti.xml")
+           (:rss/url feed)))
+    (is (= 1 (-> events-msg :events count)))
+    (is (= #{:event/name :event/data :event/happened-at :command/id}
+           (-> event keys set)))
+    (is (= :rss/created (:event/name event)))
+    (is (= "Vedomosti"
+           (get-in event [:event/data :rss/name])))
+    (is (= (local-file-url "vedomosti.xml")
+           (get-in event [:event/data :rss/url])))
+    (is (= (rss/load-news (local-file-url "vedomosti.xml"))
+           (get-in event [:event/data :rss/news])))))
